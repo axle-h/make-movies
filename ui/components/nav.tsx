194de06d498dcab3da93a281a6539bbbbfc47d0d
Alex Haslehurst <alex.haslehurst@gmail.com>
2024-08-14 11:29:03 +0100

    cleaned up navbar style

--- ui/components/nav.tsx
+++ ui/components/nav.tsx
@@ -138,47 +138,49 @@ export function Nav({ session }: { session: Session | null }) {
     </>) : <></>
 
     return (
-        <>
-            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                    <IconButton
-                        size={'md'}
-                        icon={isOpen ? <CloseIcon/> : <HamburgerIcon/>}
-                        aria-label={'Open Menu'}
-                        display={{md: 'none'}}
-                        onClick={isOpen ? onClose : onOpen}
-                    />
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} boxShadow="md">
+            <Flex height="20" alignItems="center" justifyContent="space-between">
+                <IconButton
+                    size="md"
+                    variant="ghost"
+                    icon={isOpen ? <CloseIcon/> : <HamburgerIcon/>}
+                    aria-label="Open Menu"
+                    display={{md: 'none'}}
+                    onClick={isOpen ? onClose : onOpen}
+                />
 
-                    <HStack spacing={8} alignItems={'center'}>
-                        <Box>
-                            <Link href='/'>
-                                <Image src='/icon.png' boxSize='40px' objectFit='cover' alt='make-movies'  />
-                            </Link>
-                        </Box>
-                        <HStack as={'nav'} spacing={4} display={{base: 'none', md: 'flex'}}>
-                            {navLinks}
-                        </HStack>
-                    </HStack>
-
-
-                    <Flex alignItems={'center'}>
-                        <Stack direction={'row'} spacing={7}>
-                            {!!session ? <UserMenu session={session} /> : <></>}
-                            <Button onClick={toggleColorMode}>
-                                {colorMode === 'light' ? <MoonIcon/> : <SunIcon/>}
-                            </Button>
-                        </Stack>
-                    </Flex>
-                </Flex>
-
-                {isOpen ? (
-                    <Box pb={4} display={{md: 'none'}}>
-                        <Stack as={'nav'} spacing={4}>
-                            {navLinks}
-                        </Stack>
+                <HStack spacing={8} alignItems={'center'}>
+                    <Box>
+                        <Link href='/'>
+                            <Image src='/icon.png' boxSize='40px' objectFit='cover' alt='make-movies'  />
+                        </Link>
                     </Box>
-                ) : null}
-            </Box>
-        </>
+                    <HStack as={'nav'} spacing={4} display={{base: 'none', md: 'flex'}}>
+                        {navLinks}
+                    </HStack>
+                </HStack>
+
+
+                <Flex alignItems={'center'}>
+                    <HStack spacing={{ base: '1', md: '3' }}>
+                        {!!session ? <UserMenu session={session} /> : <></>}
+                        <IconButton
+                            variant="ghost"
+                            onClick={toggleColorMode}
+                            aria-label="change colour mode"
+                            icon={colorMode === 'light' ? <MoonIcon  /> : <SunIcon />}
+                        />
+                    </HStack>
+                </Flex>
+            </Flex>
+
+            {isOpen ? (
+                <Box pb={4} display={{md: 'none'}}>
+                    <Stack as={'nav'} spacing={4}>
+                        {navLinks}
+                    </Stack>
+                </Box>
+            ) : null}
+        </Box>
     )
 }
